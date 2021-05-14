cc86ea53dd86cea04ca9b50ba22c8fa8035f197e
Artur Pabjan <artur.pabjan00@gmail.com>
2021-05-14 15:35:25 +0200

    Created component for title of section

---
+++ frontend/src/components/LineAfterTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-2">
+            <hr style="border-top: 4px solid #555;">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LineAfterTitle"
+}
+</script>
---
+++ frontend/src/components/MainTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="text-center fs-1">{{title}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainTitle",
+    props: {
+        title: String
+    }
+}
+</script>
