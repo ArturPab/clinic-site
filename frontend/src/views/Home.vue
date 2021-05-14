be357eb4e1f547d807b7291fc2fa1b07ae122979
Artur Pabjan <artur.pabjan00@gmail.com>
2021-05-14 15:36:13 +0200

    created home view

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="container my-5">
+        <MainTitle title="Nasza misja" />
+        <LineAfterTitle />
+      </div>
+      <HomeContent />
+</template>
+
+<script>
+// @ is an alias to /src
+import MainTitle from '@/components/MainTitle'
+import LineAfterTitle from '@/components/LineAfterTitle'
+import HomeContent from '@/components/home-components/HomeContent'
+
+export default {
+  name: "Home",
+  components: {
+    MainTitle,
+    LineAfterTitle,
+    HomeContent
+  }
+};
+</script>
+
+
+<style scoped>
+
+</style>
