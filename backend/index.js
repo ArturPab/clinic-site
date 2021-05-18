62ddfd1902396c4f717a55e955aad11fb8e98733
Artur Pabjan <artur.pabjan00@gmail.com>
2021-05-18 13:44:23 +0200

    created backend project

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+//Import Routes
+const authRoute = require('./routes/auth');
+const aboutUsRoute = require('./routes/about-us');
+const { required } = require('joi');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useUnifiedTopology: true, useNewUrlParser: true } ,
+    () => console.log('connected to db'));
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+app.use('/api/about-us', aboutUsRoute)
+
+
+app.listen(3000, () => console.log('Server Up and running'));
